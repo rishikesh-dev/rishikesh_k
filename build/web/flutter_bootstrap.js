cdea2d86701dffd321779c518ba1e9e57797be2e
RISHIKESH-RICHI <rishikeshdeveloperrichi@gmail.com>
2025-06-05 01:56:01 +0700

    Deploy Flutter Web clean

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "250611506"
+    serviceWorkerVersion: "4200116248"
   }
 });
